1a8283004923203945942756b99c1b6c63f850f6
Vincent M. Visconti <vviscont@SPF.local>
2017-03-20 15:53:26 -0400

    working on setup

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
 var express = require("express");
 var methodOverride = require("method-override");
-var bodyParser = requirer("body-parser");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+var PORT = process.env.PORT || 88;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// app.get('/', function(req, res){
+// 	res.send('hi')
+// });
+// Import routes and give the server access to them.
+var burgersController = require("./controllers/burgers_controller.js");
+
+app.use("/burgers", burgersController);
+
+app.listen(PORT, function(){
+	console.log('listening on PORT ' + PORT)
+});
