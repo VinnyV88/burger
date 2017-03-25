912ff0f876971700c5662d7666d8155c27d23428
vincent_visconti <vinnyv88@gmail.com>
2017-03-24 20:24:12 -0400

    implemented config.json to handle MySQL environments

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
+var path      = require("path");
+var env       = process.env.NODE_ENV || "development";
+var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+
+var connection = mysql.createConnection(config);
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burger_db"
+// });
 
 // Make connection.
 connection.connect(function (err) {
